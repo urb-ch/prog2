6fb9d99ba50acc0cd56a41a3c7393b933adb522f
urb-ch <79905058+urb-ch@users.noreply.github.com>
2021-05-11 11:52:30 +0200

    Added date and hours field, date saved as key and hours as value in JSON

--- Hours/datasave.py
+++ Hours/datasave.py
@@ -2,26 +2,28 @@ import json
 
 
 
-def speichern(datei, chosen_date):
+def speichern(datei, hours, chosen_date):
     try:
         with open(datei) as open_file:
             datei_inhalt = json.load(open_file)
     except FileNotFoundError:
         datei_inhalt = {}
 
-    datei_inhalt["Datum"] = chosen_date
+    datei_inhalt[chosen_date] = hours
 
-    print(datei_inhalt)
+    # print(datei_inhalt)
 
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file, indent=4)
 
+# def stunden_erfassen(stunden):
+  #  anzahl_stunden = stunden
+  #  print(anzahl_stunden)
 
 
-def zeit_erfassen(datum):
-    string_date = str(datum)
-    print(string_date)
-    correct_date = string_date[79:90] #ask teacher how I get the bloody Data only
-    x = {"Datum": correct_date} #currently transforming datum contents into string as DateForm is otherwise not possible to post in JSON format Object of type DateForm is not JSON serializable
+def zeit_erfassen(datum, stunden):
+    formatted_date = str(datum)
+    correct_date = formatted_date # currently transforming datum contents into string as DateForm is otherwise not possible to post in JSON format Object of type DateForm is not JSON serializable
+    correct_hours = stunden  # should be given together with correct_date as individual parameter
     datei = "datasave.json"
-    speichern(datei, x)
+    speichern(datei, correct_hours, correct_date)
--- Hours/main.py
+++ Hours/main.py
@@ -1,27 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms.fields.html5 import DateField
-from wtforms import validators, SubmitField
+from wtforms import validators, SubmitField, StringField
 import datasave
 
 app = Flask("stundenblatt")
 
-app.config["SECRET_KEY"] = "!1alpaka2?"
+app.config["SECRET_KEY"] = "!1fabod2?"  # https://github.com/CarlosIUSalazar/Python-Flask-DatePicker #Why is this needed?
 
 
-class DateForm(FlaskForm): #create class for DateForm
-    chosen_date = DateField("Chosen Date", format="%Y-%m-%d", validators=(validators.DataRequired(),)) #format the DateField and set a validator
-    submit = SubmitField("Submit") #variable for submit
+class MyForm(FlaskForm):   # create class to define what MyForm is - MyForm is based on FlaskForm
+    chosen_date = DateField("Chosen Date", format="%Y-%m-%d", validators=(validators.DataRequired(),))  # format the DateField(WTForms) and set tha a validator is required otherwise error is given
+    hours = StringField("Geben Sie ihre gearbeiteten Stunden ein:  ", validators=(validators.DataRequired(),))  # Create StringField for the hours, validator needed, currently getting an error if I put one here as well
+    submit = SubmitField("Submit")  # variable for submit
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    form = DateForm()
-    if form.validate_on_submit(): #validiert ob submit durchgeführt wurde wurde
-        datasave.zeit_erfassen(form.chosen_date) #call datasave program and give form.chosen_date
+    datum = MyForm()
+    if datum.validate_on_submit():  # validiert ob submit durchgeführt wurde wurde
+        datasave.zeit_erfassen(datum.chosen_date.data, datum.hours.data)  # call datasave program and give form.chosen_date
 
-
-    return render_template("index.html", form=form)
+    return render_template("index.html", form=datum)
 
 
 if __name__ == "__main__":
