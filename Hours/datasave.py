0f381beaaa7b54095da7760489a12886074e2bb0
urb-ch <79905058+urb-ch@users.noreply.github.com>
2021-06-09 20:52:26 +0200

    Updated code for monthly aggregation resulting in faster performance

--- Hours/datasave.py
+++ Hours/datasave.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 import calendar
 
+# as I am using the same file multiple times I opt to open it per function to circumvent accidental overwrites.
 
 def zeit_erfassen(datum, stunden):
     formatted_date = str(datum)
@@ -103,74 +104,77 @@ def monthly_aggregation():
             datei_inhalt = dict(sorted(datei_inhalt.items()))
     except FileNotFoundError:
         datei_inhalt = {}
-    test_dict = datei_inhalt
+    original_dict = datei_inhalt
 
-    year_2021 = "2021"
-    january="2021-01"
-    february="2021-02"
-    march="2021-03"
-    april="2021-04"
-    may="2021-05"
-    june="2021-06"
-    july="2021-07"
-    august="2021-09"
-    september="2021-10"
-    october="2021-10"
-    november="2021-11"
-    december="2021-12"
+    # First approach to collect monthly data - changed it to aggregating in a dictionary as it is a cleaner way rather than using the same functio over and over again
+    # year_2021 = "2021"
+    # january="2021-01"
+    # february="2021-02"
+    # march="2021-03"
+    # april="2021-04"
+    # may="2021-05"
+    # june="2021-06"
+    # july="2021-07"
+    # august="2021-09"
+    # september="2021-10"
+    # october="2021-10"
+    # november="2021-11"
+    # december="2021-12"
+    #
+    # #work further on this and print all values from testdict that containt the value string from year
+    # print("The original dictionary is: " + str(test_dict))
+    #
+    # res_jan = [val for key, val in test_dict.items() if january in key]
+    # res_jan = sum(res_jan)
+    #
+    # res_feb = [val for key, val in test_dict.items() if february in key]
+    # res_feb = sum(res_feb)
+    #
+    # res_mar = [val for key, val in test_dict.items() if march in key]
+    # res_mar = sum(res_mar)
+    #
+    # res_apr = [val for key, val in test_dict.items() if april in key]
+    # res_apr = sum(res_apr)
+    #
+    # res_may = [val for key, val in test_dict.items() if may in key]
+    # res_may = sum(res_may)
+    #
+    # res_jun = [val for key, val in test_dict.items() if june in key]
+    # res_jun = sum(res_jun)
+    #
+    # res_jul = [val for key, val in test_dict.items() if july in key]
+    # res_jul = sum(res_jul)
+    #
+    # res_aug = [val for key, val in test_dict.items() if august in key]
+    # res_aug = sum(res_aug)
+    #
+    # res_sep = [val for key, val in test_dict.items() if september in key]
+    # res_sep = sum(res_sep)
+    #
+    # res_oct = [val for key, val in test_dict.items() if october in key]
+    # res_oct = sum(res_oct)
+    #
+    # res_nov = [val for key, val in test_dict.items() if november in key]
+    # res_nov = sum(res_nov)
+    #
+    # res_dec = [val for key, val in test_dict.items() if december in key]
+    # res_dec = sum(res_dec)
+    #
+    # res_year_2021 = [val for key, val in test_dict.items() if year_2021 in key]
+    # sum_res_year_2021 = sum(res_year_2021)
+    # return res_jan, res_feb, res_mar, res_apr, res_may, res_jun, res_jul, res_aug, res_sep, res_oct, res_nov, res_dec, sum_res_year_2021,
 
-    #work further on this and print all values from testdict that containt the value string from year
-    print("The original dictionary is: " + str(test_dict))
+    # setting up an empty dictionary where data from original dict will be filled in
+    result_dict = {"2021-01": 0.00, "2021-02": 0.00, "2021-03": 0.00, "2021-04": 0.00, "2021-05": 0.00, "2021-06": 0.00,
+                   "2021-07": 0.00, "2021-08": 0.00, "2021-09":0.00, "2021-10":0.00, "2021-11":0.00, "2021-12":0.00}
+    #print("The original dictionary is: " + str(test_dict))
+    #print("The original dictionary is: " + str(result_dict))
+    for month in result_dict:
+        month_dict = {key: value for key, value in original_dict.items() if key.startswith(month)} # creating an additional month dictionary. key is key from original dict but restricted to month(key) from result dict
+        result_dict[month] = sum(month_dict.values()) #writing sum from month dict values into associated result dict (Could also do this in the step before but prefer it here)
+    #print(result_dict)
+    result_dict['2021'] = sum(result_dict.values()) #creating a new key in result dict "2021" with sum of all values as value
+    #print(result_dict)
 
-    res_jan = [val for key, val in test_dict.items() if january in key]
-    res_jan = sum(res_jan)
-
-    res_feb = [val for key, val in test_dict.items() if february in key]
-    res_feb = sum(res_feb)
-
-    res_mar = [val for key, val in test_dict.items() if march in key]
-    res_mar = sum(res_mar)
-
-    res_apr = [val for key, val in test_dict.items() if april in key]
-    res_apr = sum(res_apr)
-
-    res_may = [val for key, val in test_dict.items() if may in key]
-    res_may = sum(res_may)
-
-    res_jun = [val for key, val in test_dict.items() if june in key]
-    res_jun = sum(res_jun)
-
-    res_jul = [val for key, val in test_dict.items() if july in key]
-    res_jul = sum(res_jul)
-
-    res_aug = [val for key, val in test_dict.items() if august in key]
-    res_aug = sum(res_aug)
-
-    res_sep = [val for key, val in test_dict.items() if september in key]
-    res_sep = sum(res_sep)
-
-    res_oct = [val for key, val in test_dict.items() if october in key]
-    res_oct = sum(res_oct)
-
-    res_nov = [val for key, val in test_dict.items() if november in key]
-    res_nov = sum(res_nov)
-
-    res_dec = [val for key, val in test_dict.items() if december in key]
-    res_dec = sum(res_dec)
-
-    res_year_2021 = [val for key, val in test_dict.items() if year_2021 in key]
-    sum_res_year_2021 = sum(res_year_2021)
-    print("Total worked hours in January: " + str(res_jan))
-    print("Total worked hours in February: " + str(res_feb))
-    print("Total worked hours in March: " + str(res_mar))
-    print("Total worked hours in April: " + str(res_apr))
-    print("Total worked hours in May: " + str(res_may))
-    print("Total worked hours in June: " + str(res_jun))
-    print("Total worked hours in July: " + str(res_jul))
-    print("Total worked hours in August: " + str(res_aug))
-    print("Total worked hours in September: " + str(res_sep))
-    print("Total worked hours in October: " + str(res_oct))
-    print("Total worked hours in November: " + str(res_nov))
-    print("Total worked hours in December: " + str(res_dec))
-    print("Total worked hours in 2021: " + str(sum_res_year_2021))
-    return res_jan, res_feb, res_mar, res_apr, res_may, res_jun, res_jul, res_aug, res_sep, res_oct, res_nov, res_dec, sum_res_year_2021
+    # returning dict as list
+    return list(result_dict.values())
